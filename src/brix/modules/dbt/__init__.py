f226704e926062a090c8cd65efb1dbdc2370ddda
Spycner <pascal98kraus@gmail.com>
2025-11-27 17:11:20 +0100

    fix: update default logging level to ERROR in LogConfig

--- src/brix/modules/dbt/__init__.py
+++ src/brix/modules/dbt/__init__.py
@@ -4,4 +4,4 @@ from brix.modules.dbt.passthrough import pre_dbt_hook, run_dbt
 from brix.modules.dbt.profile import ProfileExistsError, init_profile
 from brix.modules.dbt.profile_models import DbtProfiles
 
-__all__ = ["pre_dbt_hook", "run_dbt", "init_profile", "ProfileExistsError", "DbtProfiles"]
+__all__ = ["DbtProfiles", "ProfileExistsError", "init_profile", "pre_dbt_hook", "run_dbt"]
